41e6e0a8e52c3de978cd4d3b8b8be6d9a847d3ad
akshayaher72 <aherakshay539@gmail.com>
2025-03-02 10:22:33 +0530

    Update mult.cpp

--- mult.cpp
+++ mult.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 using namespace std;
-int mult(int a, int b) {
+
+class mult{
+    
+    public:
+
+     mult(int a, int b) {
      int c = a * b;
          cout << " two int mult: " << c << endl;
 
 }
-int mult(int a, int b, int c) {
+    mult(int a, int b, int c) {
     int d= a * b * c;
         cout << " three int mult : " << d << endl;
 
 }
-float mult(float a, float b) {
+    mult(float a, float b) {
     float c= a * b;
          cout << " two float mult: " << c << endl;
 
 }
+};
 
 int main() {
-    int m1 = mult(3, 7);
-    int m2 = mult(0, 2, 9);
-    float m3 = mult(8.1f, 6.2f);
+    mult m1(3, 7);
+    mult m2(0, 2, 9);
+    mult m3(8.1f, 6.2f);
 
     return 0;
 }
